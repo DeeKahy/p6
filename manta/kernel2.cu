7b8c5fd6c006692dfbeb13f43c5752aa0de022b3
mantarias <32459446+mantarias@users.noreply.github.com>
2025-03-25 12:09:56 +0100

    removed unnecessary things

--- manta/kernel2.cu
+++ manta/kernel2.cu
@@ -123,14 +123,8 @@ int main(int argc, char *argv[]) {
 	int gridSize = argc > 1 ? std::stoi(argv[1]) : 1000;
 	int blockSize = 1024;
 	int numSimulations = gridSize * blockSize;
-	
-	float* results = new float[numSimulations];
 	float* d_results;
 	cudaMalloc((void**)&d_results, numSimulations * sizeof(float));
-	curandState* d_states;
-	cudaMalloc((void**)&d_states, numSimulations * sizeof(curandState));
-	initCurandStates << <gridSize, blockSize >> > (d_states, time(0));
-	cudaDeviceSynchronize();
 	initThread << <gridSize, blockSize >> > (d_results);
 	cudaDeviceSynchronize();
 
@@ -139,9 +133,7 @@ int main(int argc, char *argv[]) {
 	sum << <1, 1 >> > (d_results, numSimulations);
 	cudaDeviceSynchronize();
 	std::cout << "True value of e: 2.71828... \n";
-	delete[] results;
 	cudaFree(d_results);
-	cudaFree(d_states);
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 	std::cout << "time run: " << duration.count() << std::endl;
